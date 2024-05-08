d0510752004fc141a37c110235549ec2594d2a46
ahu <2659813569@qq.com>
2024-05-08 20:50:30 +0800

    20240508

---
+++ spring/springboot2/springboot2Pure/src/main/java/org/example/annatationLearn/ParamValidatorAdvice.java
@@ -0,0 +1,7 @@
+package org.example.annatationLearn;
+
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+
+public class ParamValidatorAdvice {
+}
--- spring/springboot2/springboot2Pure/src/main/java/org/example/springboot2PureApplication.java
+++ spring/springboot2/springboot2Pure/src/main/java/org/example/springboot2PureApplication.java
@@ -3,12 +3,14 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@EnableAspectJAutoProxy
 public class springboot2PureApplication
 {
     public static void main(String[] args) {
